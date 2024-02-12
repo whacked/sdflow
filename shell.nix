35c7ec29d15b4b0ce605ba9ad7853534edbb1ce2
whacked-xps <whacked@users.noreply.github.com>
2024-02-13 01:09:27 +0800

    bundle go-jsonschema into shell env

--- shell.nix
+++ shell.nix
@@ -15,6 +15,19 @@
 
 let
   goEnv = mkGoEnv { pwd = ./.; };
+  go-jsonschema = pkgs.stdenv.mkDerivation {
+    name = "go-jsonschema";
+    src = pkgs.fetchurl {
+      url = "https://github.com/omissis/go-jsonschema/releases/download/v0.15.0/go-jsonschema_Linux_x86_64.tar.gz";
+      sha256 = "diR8EUGrEcVyhW5kAyDyHluoWRnj3lUlNL2BbhUjFS4=";
+    };
+    dontUnpack = true;
+    installPhase = ''
+      mkdir -p $out/bin
+      tar -xzf $src -C $out/bin
+    '';
+    buildInputs = [ pkgs.unzip ];
+  };
 in
 pkgs.mkShell {
   packages = [
@@ -23,5 +36,10 @@ pkgs.mkShell {
     pkgs.check-jsonschema
     pkgs.jsonnet
     pkgs.jq
+    go-jsonschema
   ];
+
+  shellHook = ''
+    source bash_autocomplete.sh
+  '';
 }
